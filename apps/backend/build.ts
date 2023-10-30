2edd514ce7dd9460847801d11b1321b396c2711f
Atiwat Seenark <um_oom@hotmail.com>
2023-10-30 18:31:16 +0700

    :bug: fix(turborepo): prettier
    use tabWidth=4

--- apps/backend/build.ts
+++ apps/backend/build.ts
@@ -1,8 +1,8 @@
 Bun.build({
-  entrypoints: ["./src/index.ts"],
-  outdir: "dist",
-  sourcemap: "external",
-  target: "bun",
-  format: "esm",
-  minify: true
+    entrypoints: ["./src/index.ts"],
+    outdir: "dist",
+    sourcemap: "external",
+    target: "bun",
+    format: "esm",
+    minify: true
 });
--- packages/elysia-app/src/index.ts
+++ packages/elysia-app/src/index.ts
@@ -3,5 +3,5 @@ import { Elysia } from "elysia";
 export const app = new Elysia().get("/", () => "Hello Elysia");
 
 app.get("/healthz", () => {
-  return "Ok Naja";
+    return "Ok Naja";
 });
